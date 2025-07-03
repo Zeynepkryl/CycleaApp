4e47e156e11a0512f1f3d57869704f2ba9293b05
zeynep <zeynep.karayel@tigahealth.com>
2025-07-03 16:52:18 +0300

    UseCase.kt created

--- app/src/main/java/com/example/cycleaapp/core/UseCase.kt
+++ app/src/main/java/com/example/cycleaapp/core/UseCase.kt
@@ -1,4 +1,28 @@
 package com.example.cycleaapp.core
 
-class UseCase {
+/**
+ * Base UseCase class encapsulating business logic execution.
+ * @param P - input parameters type
+ * @param R - return type
+ */
+abstract class UseCase<in P, R> {
+
+    /**
+     * Invoke this UseCase with given parameters.
+     */
+    suspend operator fun invoke(parameters: P): R = execute(parameters)
+
+    /**
+     * Override this to implement the use case logic.
+     * @param parameters input parameters of type P
+     * @return result of type R
+     */
+    @Throws(Exception::class)
+    protected abstract suspend fun execute(parameters: P): R
 }
+
+/**
+ * Extension for UseCase without parameters (Unit).
+ * Allows calling useCase() instead of useCase(Unit).
+ */
+suspend operator fun <R> UseCase<Unit, R>.invoke(): R = this(Unit)
