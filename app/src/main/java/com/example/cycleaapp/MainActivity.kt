ea1a00bbf03870a06097c4867cc679284ddbdea7
zeynep <zeynep.karayel@tigahealth.com>
2025-07-03 10:26:25 +0300

    initial commit

--- app/src/main/java/com/example/cycleaapp/MainActivity.kt
+++ app/src/main/java/com/example/cycleaapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.cycleaapp.ui.theme.CycleaAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,8 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CycleaAppTheme {
-                val vm: MyViewModel = hiltViewModel()
-                Text("Hello Compose + Hilt")
+                Text("Hello World")
             }
         }
     }
