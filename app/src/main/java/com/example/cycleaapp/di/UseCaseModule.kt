51652bd7200ed6357c9867a1df0270b3152ac55a
zeynep <zeynep.karayel@tigahealth.com>
2025-07-03 16:52:52 +0300

    UseCaseModule.kt created

--- app/src/main/java/com/example/cycleaapp/di/UseCaseModule.kt
+++ app/src/main/java/com/example/cycleaapp/di/UseCaseModule.kt
@@ -1,4 +1,25 @@
 package com.example.cycleaapp.di
 
-class UseCaseModule {
+import com.example.cycleaapp.domain.usecase.GetCurrentUser
+import com.example.cycleaapp.domain.usecase.InitializeAppCache
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Hilt module for providing UseCase instances.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Singleton
+    @Provides
+    fun provideInitializeAppCache(useCase: InitializeAppCache): InitializeAppCache = useCase
+
+    @Singleton
+    @Provides
+    fun provideGetCurrentUser(useCase: GetCurrentUser): GetCurrentUser = useCase
 }
